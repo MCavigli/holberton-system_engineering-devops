2fdff79122e86c94649b21b7d34422f48e495f44
Marc Cavigli <mcavigli@gmail.com>
2019-07-09 04:52:15 +0000

    Change \w to \S

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:)\+?\w*(?<!\])|(?<=to:)\+?\d*(?<!\])|(?<=flags:).{9,14}(?<!\])/).join(',')
+puts ARGV[0].scan(/(?<=from:)\+?\S*(?<!\])|(?<=to:)\+?\d*(?<!\])|(?<=flags:).{9,14}(?<!\])/).join(',')
