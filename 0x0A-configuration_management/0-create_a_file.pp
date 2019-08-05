8b78750a6b50c6e0183dcbd7a9b23d671d3809d6
Marc Cavigli <mcavigli@gmail.com>
2019-08-05 18:33:44 +0000

    Fixed puppet-lint issues

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Create a file in /tmp with various attributes
 
 file { '/tmp/holberton':
-     ensure	=> file,
-     path	=> '/tmp/holberton',
-     mode	=> '0744',
-     owner	=> 'www-data',
-     group	=> 'www-data',
-     content	=> 'I love Puppet'
-     }
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+  }
