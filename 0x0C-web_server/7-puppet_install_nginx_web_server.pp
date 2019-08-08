9fa8a917cd49cb122c8cf00ec926d88282c94873
Marc Cavigli <mcavigli@gmail.com>
2019-08-08 20:29:34 +0000

    Pushed Task 6 to directory

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+# install and configure an Nginx server using Puppet instead of Bash
+
+package { 'nginx':
+  ensure => installed,
+  name   => 'nginx'
+}
+file { 'path_to_html':
+  content => 'Holberton School',
+  path    => '/var/www/html/index.nginx-debian.html'
+}
+file_line { 'put_in_line':
+  ensure => present,
+  path   => 'etc/nginx/sites-available/default',
+  after  => 'server_name_;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+}
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx']
+}
