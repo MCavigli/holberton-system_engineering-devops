f14fb540914a2c862a89ca037d6e1043030eca6f
Marc Cavigli <mcavigli@gmail.com>
2019-09-19 22:31:57 +0000

    Initial push

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# Recursive call returns list of titles for hot articles
+import requests
+
+
+def recurse(subreddit, hot_list=[], count=0, after_val=None):
+    '''returns list of titles for hot articles'''
+    URL = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    USERAGENT = {'User-Agent':
+                 'Unix:com.holberton.apiadvanced:task0 (by /u/_marc_marc_)'}
+    params = {'limit': 100}
+    if count == 0:
+        req = requests.get(URL,
+                           headers=USERAGENT,
+                           params=params)
+    else:
+        params['after'] = after_val
+        req = requests.get(URL,
+                           headers=USERAGENT,
+                           params=params)
+    if req.status_code is not 200:
+        print(None)
+        return
+    jreq = req.json()
+    data_path = jreq['data']['children']
+    for i in range(len(data_path)):
+        hot_list.append(data_path[i]['data']['title'])
+    count += 1
+    after_val = jreq['data']['after']
+    if after_val is None:
+        print(len(hot_list))
+        return (hot_list)
+    else:
+        return (recurse(subreddit, hot_list, count, after_val))
