4bc6ec5f60dcfd135789a4c50c3adb508f0ab13f
Marc Cavigli <mcavigli@gmail.com>
2019-09-19 21:00:42 +0000

    Initial push

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Module that prints top 10 titles
+import requests
+
+
+def top_ten(subreddit):
+    '''Prints titles of top 10 hot posts for subreddit'''
+    URL = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    USERAGENT = {'User-Agent':
+                 'Unix:com.holberton.apiadvanced:task1 (by /u/_marc_marc_)'}
+    req = requests.get(URL, headers=USERAGENT)
+    if req.status_code is not 200:
+        return (0)
+    jreq = req.json()
+    data_path = jreq['data']['children']
+    for i in range(10):
+        print(data_path[i]['data']['title'])
