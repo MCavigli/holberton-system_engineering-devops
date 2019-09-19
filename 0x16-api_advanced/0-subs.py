a47a35e346fe1b5a806d5bc910b73b2b09eb3a27
Marc Cavigli <mcavigli@gmail.com>
2019-09-19 20:40:21 +0000

    Fixed code to look for good status code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,8 @@ def number_of_subscribers(subreddit):
     URL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     USERAGENT = {'User-Agent':
                  'Unix:com.holberton.apiadvanced:task0 (by /u/_marc_marc_)'}
-    req = requests.get(URL, headers=USERAGENT).json()
-    if req['error'] == 404:
+    req = requests.get(URL, headers=USERAGENT)
+    if req.status_code is not 200:
         return (0)
-    return (req['data']['subscribers'])
+    jreq = req.json()
+    return (jreq['data']['subscribers'])
