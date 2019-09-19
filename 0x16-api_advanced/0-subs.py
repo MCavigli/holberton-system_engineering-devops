c5aa1f760f6836810f4a5ca4097604c0c4e86199
Marc Cavigli <mcavigli@gmail.com>
2019-09-19 20:31:07 +0000

    Initial push

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Finds total subscribers to a subreddit
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''Function that returns total subscriber number'''
+    URL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    USERAGENT = {'User-Agent':
+                 'Unix:com.holberton.apiadvanced:task0 (by /u/_marc_marc_)'}
+    req = requests.get(URL, headers=USERAGENT).json()
+    return (req['data']['subscribers'])
