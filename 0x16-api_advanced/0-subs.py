3a8c2161143efdb80e411330d93e357a83dc7741
Marc Cavigli <mcavigli@gmail.com>
2019-09-19 20:35:05 +0000

    Added conditional to check for fake subreddits

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,4 +9,6 @@ def number_of_subscribers(subreddit):
     USERAGENT = {'User-Agent':
                  'Unix:com.holberton.apiadvanced:task0 (by /u/_marc_marc_)'}
     req = requests.get(URL, headers=USERAGENT).json()
+    if req['error'] == 404:
+        return (0)
     return (req['data']['subscribers'])
