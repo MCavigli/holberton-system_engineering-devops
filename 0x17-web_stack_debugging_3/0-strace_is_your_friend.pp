bf772862a36be90f8c70123ede2a59faf17dbe33
Marc Cavigli <mcavigli@gmail.com>
2019-10-02 22:07:03 +0000

    Removed line number from sed command and used a replace every instance

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Corrects a typo in wordpress settings and fixes 500 error
 exec { 'fix_typo':
-  command =>   "sed 137 's/phpp/php/' /var/www/html/wp-settings.php",
+  command =>   "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    =>   '/bin'
 }
